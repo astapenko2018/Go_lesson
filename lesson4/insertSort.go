e0ecb44b56a0cc6c891c908dcb9fdc29c8e69347
Oleg Astapenko <sepultura90@inbox.ru>
2022-08-29 18:39:15 +0300

    Create insertSort.go
    add task4

---
+++ lesson4/insertSort.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func insertionSort(origSlice [] int) []int {
+	lenSlice := len(origSlice)
+	res := make([]int, lenSlice)
+	copy(res, origSlice)
+
+	for i := 1, i < lenSlice; i ++{
+		fpo
+	}
+}
